c9480a267335bb4583a1380350ecd6c64ea22dad
pindrive <kamilniewiadomski18@gmail.com>
2025-01-26 13:19:56 +0100

    feat: initialize work on Bookfolio mobile app for Android!

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Tabs } from 'expo-router';
+
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Tab One',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: 'Tab Two',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
